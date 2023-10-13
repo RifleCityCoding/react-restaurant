f34056fe408d17f92767776e5cb1ed7cd297b437
RifleCityCoding <142933516+RifleCityCoding@users.noreply.github.com>
2023-10-13 10:01:07 -0400

    adjusting accordion and card

--- le-roux-effronte/app/page.js
+++ le-roux-effronte/app/page.js
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 import styles from "./page.module.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import MainHeader from "./MainHeader";
+import MenuAccord from "./MenuAccord";
+import { Main } from "next/document";
+import MenuCard from "./MenuCard";
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -19,18 +22,32 @@ export default function Home() {
         console.error("Error fetching data:", error);
       });
   }, []);
-
-  
+console.log(data);
   return (
-    <main style={{
-      backgroundImage: `url("https://images8.alphacoders.com/875/875387.png")`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      width: '100vw',
-      height: '200vh',
-    }}>
-      <MainHeader />
+    <main
+      style={{
+        backgroundImage: `url("https://images8.alphacoders.com/875/875387.png")`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        width: "100vw",
+        height: "200vh",
+      }}
+    >
+      <div className="Container">
+        <div className="Row">
+          <div className="Col">
+            <MainHeader />
+
+           <MenuCard data={data}>
+           </MenuCard>
+           
+          
+          
+          
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
